daa65edac311aad383895efd2b449bdeec4918d8
Recep Öztürk <turkishteacher23@gmail.com>
2020-02-19 11:37:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function BoardMember(name,homeState,training){
   this.training=training,
   
   this.veto=function(){
+<<<<<<< HEAD
     return `No, I must disagree`
   },
   this.approve=function(){
@@ -17,5 +18,17 @@ function BoardMember(name,homeState,training){
   },
   this.sayHi=function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+=======
+    return `I must disagree`
+  },
+  this.approve=function(){
+    return `You can do that`
+  }
+  this.doCharity=function(){
+    return `You will see great things from Scuber.`
+  }
+  this.sayHi=function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`
+>>>>>>> 8abf0f272d4f6caf6dfb3b905cf0c03906f8bb4f
   }
 }
